c40adb398482d38d2500b1da9a9d4f47ffee7dac
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-11-01 14:04:32 +0700

    Prepare for lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Bai1Component } from './bai1/bai1.component';
-import { Bai2Component } from './bai2/bai2.component';
-import { Bai3Component } from './bai3/bai3.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  declarations: [
-    Bai1Component,
-    Bai2Component,
-    Bai3Component
-  ],
-  exports: [
-    Bai1Component,
-    Bai2Component,
-    Bai3Component
-  ],
+  imports: [CommonModule, FormsModule],
+  declarations: [],
+  exports: [],
 })
 export class AppModule {}
