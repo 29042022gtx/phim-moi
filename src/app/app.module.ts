6f4234fd127ecd97eef2c9a3232779c344805dfd
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-11-04 14:24:39 +0700

    Prepare for lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Bai1Component } from './bai1/bai1.component';
-import { Bai2Component } from './bai2/bai2.component';
-import { RouterOutlet } from '@angular/router';
-import { Bai3Component } from './bai3/bai3.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, RouterOutlet],
-  declarations: [
-    Bai1Component,
-    Bai2Component,
-    Bai3Component
-  ],
-  exports: [
-    Bai1Component,
-    Bai2Component,
-    Bai3Component
-  ],
+  imports: [CommonModule, FormsModule],
+  declarations: [],
+  exports: [],
 })
 export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,3 @@
 import { Routes } from '@angular/router';
-import { Bai2Component } from './bai2/bai2.component';
-import { Bai1Component } from './bai1/bai1.component';
-import { Bai3Component } from './bai3/bai3.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: Bai1Component,
-  },
-  {
-    path: 'bai1',
-    component: Bai1Component,
-  },
-  {
-    path: 'bai2',
-    component: Bai2Component,
-  },
-  {
-    path: 'bai3',
-    component: Bai3Component,
-  },
-];
+export const routes: Routes = [];
