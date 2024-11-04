6da32917ed51b5ce71a77752de8291601ef28a63
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-11-04 14:15:33 +0700

    Prepare for lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { Bai1StarComponent } from './bai1-star/bai1-star.component';
-import { Bai1ProductListComponent } from './bai1-product-list/bai1-product-list.component';
-import { Bai2ProductListComponent } from './bai2-product-list/bai2-product-list.component';
-import { Bai2ProductDetailsComponent } from './bai2-product-details/bai2-product-details.component';
-import { Bai3ProductListComponent } from './bai3-product-list/bai3-product-list.component';
-import { Bai3ProductDetailsComponent } from './bai3-product-details/bai3-product-details.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterOutlet, RouterLink],
-  declarations: [
-    Bai1StarComponent,
-    Bai1ProductListComponent,
-    Bai2ProductListComponent,
-    Bai2ProductDetailsComponent,
-    Bai3ProductListComponent,
-    Bai3ProductDetailsComponent,
-  ],
-  exports: [
-    Bai1StarComponent,
-    Bai1ProductListComponent,
-    Bai2ProductListComponent,
-    Bai2ProductDetailsComponent,
-    Bai3ProductListComponent,
-    Bai3ProductDetailsComponent,
-  ],
+  imports: [CommonModule, FormsModule],
+  declarations: [],
+  exports: [],
 })
 export class AppModule {}
